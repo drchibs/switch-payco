1244a1830e5e392cd898b3117ee517b2e86e13f7
D.R Chibs <rdchibs@gmail.com>
2024-08-25 11:40:53 +0100

    initial commit

---
+++ src/main/java/com/chibs/payco/PaycoApplication.java
@@ -0,0 +1,13 @@
+package com.chibs.payco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaycoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chibs/payco/PaycoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibs.payco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaycoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
